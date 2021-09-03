b5267e989b24683e176ca860246e0b7ddeb8b4c4
JOÃO GABRIEL COSTA FLÔR <21276663@CFP127ALUNO.LOCAL>
2021-09-03 16:56:08 -0300

    Adicionada area da casa

---
+++ src/areadaCasa2/AreadaCasa.java
@@ -0,0 +1,21 @@
+package areadaCasa2;
+
+import java.util.Scanner;
+
+public class AreadaCasa {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		String comodo;
+		
+		System.out.println("Qual o nome do cômodo? ");
+		comodo = leitor.next();
+		System.out.println("Qual a largura ");
+		
+		
+
+	}
+
+}
