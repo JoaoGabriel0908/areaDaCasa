26534b9fc5d8554bd98a0a75ea9d2b2ff14afa9d
JOÃO GABRIEL COSTA FLÔR <21276663@CFP127ALUNO.LOCAL>
2021-09-09 16:07:09 -0300

    Adicionado atualização Area da Casa

--- src/areadaCasa2/AreadaCasa.java
+++ src/areadaCasa2/AreadaCasa.java
@@ -8,14 +8,36 @@ public class AreadaCasa {
 		
 		Scanner leitor = new Scanner (System.in);
 		
-		String comodo;
+		String comodo,resposta = "s";
+		double largura,comprimento,metros,area=0;
+	
+		System.out.print("Você deseja calcular? ");
+		resposta=leitor.next();
 		
-		System.out.println("Qual o nome do cômodo? ");
-		comodo = leitor.next();
-		System.out.println("Qual a largura ");
+		if(resposta.equals("s")) {
+			
+			while (resposta.equals("s")) {
+				System.out.print("Qual o nome do cômodo? ");
+				comodo = leitor.next();
+				System.out.print("Qual a largura? ");
+				largura = leitor.nextDouble();
+				System.out.print("Qual o comprimento? ");
+				comprimento = leitor.nextDouble();
+				
+				metros=largura*comprimento;
+				System.out.println("A " + comodo + " tem " + metros + " metros quadrados ");
+			
+				System.out.println("Você deseja calcular mais cômodos(s/n)? ");
+				resposta = leitor.next();
+				
+				area=metros+area;
+				
+				
+			}
 		
+			leitor.close();
 		
-
+			System.out.println(" A área total da casa é " + area + "metros quadrados");
+		}
 	}
-
 }
